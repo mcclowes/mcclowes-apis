8838df10dba83a5c9f3ac6dfb754cc3f509a17cf
Max Clayton Clowes <contact@mcclowes.com>
2025-01-02 11:37:39 +0000

    Add hash

--- index.js
+++ index.js
@@ -31,14 +31,14 @@ app.get(`/todos/due${process.env.HASH}`, async (req, res) => await getAndSend(re
 
 app.get(`/todos/summarize${process.env.HASH}`, async (req, res) => await getAndSend(req, res, todoist.summarize))
 
-app.get(`/todos/process/reprioritize`, async (req, res) => await getAndSend(req, res, todoist.reprioritize))
+app.get(`/todos/process/reprioritize${process.env.HASH}`, async (req, res) => await getAndSend(req, res, todoist.reprioritize))
 
-app.get(`/todos/process/stale`, async (req, res) => await getAndSend(req, res, todoist.killOld))
+app.get(`/todos/process/stale${process.env.HASH}`, async (req, res) => await getAndSend(req, res, todoist.killOld))
 
-app.get(`/todos/process/categorize`, async (req, res) => await getAndSend(req, res, todoist.categorize))
+app.get(`/todos/process/categorize${process.env.HASH}`, async (req, res) => await getAndSend(req, res, todoist.categorize))
 
-app.get(`/todos/process/new-day`, async (req, res) => await getAndSend(req, res, todoist.newDay))
+app.get(`/todos/process/new-day${process.env.HASH}`, async (req, res) => await getAndSend(req, res, todoist.newDay))
 
-app.get(`/todos/process/new-day-focus`, async (req, res) => await getAndSend(req, res, todoist.newDayFocus))
+app.get(`/todos/process/new-day-focus${process.env.HASH}`, async (req, res) => await getAndSend(req, res, todoist.newDayFocus))
 
 app.listen(3000, () => console.log('LISTENING ON 3000'))
